4c26f005d484b8277db5fe62525e1a8a18925c06
shajim <seenishajim_khan@infosys.com>
2018-05-04 18:03:03 +0530

    check 33

--- webhook.php
+++ webhook.php
@@ -16,19 +16,20 @@ foreach ( $input['details'] as $key => $value) {
     }
 // encode post_data into json
 $sendInput=json_encode($post_data, true);
-error_log('info_log:$sendInput'.$sendInput);
-error_log('info_log:$sendInput'.$sendInput['Phone']);
 $postInput = json_decode($sendInput, true);
 $postInputValue=implode("&",$postInput);
-error_log('info_log:$$inputString'.$postInputValue);
+error_log('info_log:$postInputValue'.$postInputValue);
 //create cURL connection
 $curl_connection = curl_init('https://kohler.leadperfection.com/batch/leadformgenerictest.asp');
 //set data to be posted
-curl_setopt( $curl_connection, CURLOPT_POST, 1);
+curl_setopt($curl_connection, CURLOPT_POST, 1);
 curl_setopt($curl_connection, CURLOPT_POSTFIELDS, $postInputValue);
+curl_setopt($curl_connection, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($curl_connection, CURLOPT_HEADER, 0);
+curl_setopt($curl_connection, CURLOPT_RETURNTRANSFER, 1);
 //perform our request
-$result = curl_exec($curl_connection);
-error_log('info_log:$result'.$result);
+$response = curl_exec($curl_connection);
+error_log('info_log:$response'.$response);
 //close the connection
 curl_close($curl_connection);
 
