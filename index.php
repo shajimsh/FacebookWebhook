8dc089a882e687d1af0b671b2743c8ac36cb85a9
shajim <seenishajim_khan@infosys.com>
2018-05-08 12:40:16 +0530

    added in index

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 <?php
 
-echo "<p>This is Kohler application for integrating Facebook lead ads via a webhook with KOHLER LPL appication.</p><br /> ";
-echo "<br />";
+echo "<div><p>This is Kohler application for integrating Facebook lead ads via a webhook with KOHLER LPL appication.</p> ";
 echo "<p>Application is hosted on Azure app server   Kohler Subscription is Corporate Communications - 1</p> <br /> ";
-echo "<br /><p> LPL application : https://kohler.leadperfection.com/batch/leadformgeneric.asp </p> ";
-echo "<p>Facebook page is Kohler Company <br /> </p>" ;
-echo "<p>Bit Bucket repository is https://bitbucket.org/kohlerinfy/leadadswebhooklpl/ </p><br /> ";
+echo "<p> LPL application : <a href='https://kohler.leadperfection.com/batch/leadformgeneric.asp'/> </p> ";
+echo "<p>Facebook page is Kohler Company </p>" ;
+echo "<p>Bit Bucket repository is <a href='https://bitbucket.org/kohlerinfy/leadadswebhooklpl/'/> </p>";
 echo "<p>STOFFLE MATTHAUS <Matt.Stoffle@kohler.com> - Contact for Azure app service and Bit Bucket repository.</p>";
-echo " <p>FRANK-ARLT MARY <MARY.FRANK-ARLT@kohler.com> for Facebook webhook app .</p>"
+echo " <p>FRANK-ARLT MARY <MARY.FRANK-ARLT@kohler.com> for Facebook webhook app .</p></div>"
 ?>
