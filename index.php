c20c77eadd78ffe3691fd335d9d01dfc7cfb6797
shajim <seenishajim_khan@infosys.com>
2018-05-08 14:39:38 +0530

    added

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 
 echo "<div><p>This is Kohler application for integrating Facebook lead ads via a webhook with KOHLER LPL appication.</p> ";
 echo "<p>Application is hosted on Azure app server   Kohler Subscription is Corporate Communications - 1</p> <br /> ";
-echo "<p> LPL application : <a href='https://kohler.leadperfection.com/batch/leadformgeneric.asp'></a> </p> ";
+echo "<p> LPL application : <a href='https://kohler.leadperfection.com/batch/leadformgeneric.asp'></a></p> ";
 echo "<p>Facebook page is Kohler Company </p>" ;
-echo "<p>Bit Bucket repository is <a href='https://bitbucket.org/kohlerinfy/leadadswebhooklpl/'> </a> </p>";
+echo "<p>Bit Bucket repository is <a href='https://bitbucket.org/kohlerinfy/leadadswebhooklpl/'></a></p>";
 echo "<p>STOFFLE MATTHAUS <Matt.Stoffle@kohler.com> - Contact for Azure app service and Bit Bucket repository.</p>";
 echo " <p>FRANK-ARLT MARY <MARY.FRANK-ARLT@kohler.com> for Facebook webhook app .</p></div>"
 ?>
